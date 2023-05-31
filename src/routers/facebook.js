f460d5af977b8606ea51e5e7ffbd467529dad4c8
Rahul Anand <rahul03061998@gmail.com>
2023-05-31 17:26:37 +0530

    video get separate API

--- src/routers/facebook.js
+++ src/routers/facebook.js
@@ -1,4 +1,4 @@
-const {create_campaign, get_campaign,create_adSet,get_adSet,create_creative,get_creative,create_ad, get_account_pages, get_location_keys,create_creative_video_upload,create_creative_video} = require("../controllers/facebook");
+const {create_campaign, get_campaign,create_adSet,get_adSet,create_creative,get_creative,create_ad, get_account_pages, get_location_keys,create_creative_video_upload,create_creative_video, get_page_video} = require("../controllers/facebook");
 const authenticateToken = require('../middleware/auth')
 const express = require("express");
 const router = express.Router();
@@ -14,5 +14,6 @@ router.get("/user/account/pages",get_account_pages);
 router.get("/location",get_location_keys);
 router.post("/ad-creative/upload/create_video", create_creative_video_upload);
 router.post("/ad-creative/create_video", create_creative_video);
+router.get("/videos",get_page_video)
 
 module.exports = router;
